4e10be892ede372a209b648ac67fbc2e13c6e4ec
zhuzichu <zhuzichu520@gmail.com>
2022-01-14 00:43:21 +0800

    update

--- src/ui/layout/Main.qml
+++ src/ui/layout/Main.qml
@@ -1,7 +1,6 @@
 ﻿import QtQuick 2.15
 import QtQuick.Controls 2.15
 import QtQuick.Window 2.15
-import QtGraphicalEffects 1.0
 import "../component"
 
 CusWindow {
@@ -14,26 +13,6 @@ CusWindow {
 
     CusSliderBar{
         id:slider
-    }
-
-    Rectangle {
-        id:content
-        height: parent.height
-        anchors.left: slider.right
-        anchors.right: parent.right
-        color: "#f9f9f9"
-    }
-
-    InnerShadow {
-        anchors.fill: content
-        cached: true
-        horizontalOffset: 1
-        verticalOffset: 0
-        radius: 4
-        samples: 32
-        color: "#10000000"
-        smooth: true
-        source: content
     }
 
 }
--- src/ui/main.cpp
+++ src/ui/main.cpp
@@ -1,6 +1,7 @@
 ﻿#include <QGuiApplication>
 #include <QQmlApplicationEngine>
 #include <QtQuick/qquickwindow.h>
+#include <QApplication>
 
 #include "MainWindow.h"
 
@@ -9,7 +10,9 @@ int main(int argc, char *argv[])
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
-//    QGuiApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
+//    QCoreApplication::setAttribute(Qt::AA_ShareOpenGLContexts);
+    //    QCoreApplication::setAttribute(Qt::AA_UseSoftwareOpenGL);
+    QQuickWindow::setSceneGraphBackend(QSGRendererInterface::Software);
     QGuiApplication app(argc, argv);
     MainWindow window;
     window.show();
